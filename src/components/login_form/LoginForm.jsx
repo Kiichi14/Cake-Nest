0a12f6c2500aedbf763d46e013973d95015e6def
Kiichi14 <100412917+Kiichi14@users.noreply.github.com>
2023-11-21 14:08:06 +0100

    F03 LoginPage > style page and install react-icons library

--- src/components/login_form/LoginForm.jsx
+++ src/components/login_form/LoginForm.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from 'styled-components';
+import { theme } from "../../theme/Theme";
 
 function LoginForm() {
 
@@ -24,12 +26,12 @@ function LoginForm() {
     return (
         <>
             <div className="login-form-container">
-                <h3>Connectez-vous</h3>
+                <SubTitle>Connectez-vous</SubTitle>
                 <form action="submit" onSubmit={handleSubmit}>
-                    <div className="form-group">
-                        <input type="text" name="firstname" placeholder="Entrez votre prénom" value={firstName} onChange={handleChange}/>
-                        <button type="submit">Mon espace</button>
-                    </div>
+                    <FormLogin>
+                        <FormInput type="text" name="firstname" placeholder="Entrez votre prénom" value={firstName} onChange={handleChange}></FormInput>
+                        <FormButton type="submit">Mon espace</FormButton>
+                    </FormLogin>
                 </form>
             </div>
         </>
@@ -37,3 +39,32 @@ function LoginForm() {
 }
 
 export default LoginForm;
+
+const SubTitle = styled.h3 `
+    color: ${theme.colors.white};
+    font-family: 'Pacifico', cursive;
+    font-size: ${theme.fonts.size.P4};
+    padding: ${theme.spacing.sm};
+    text-align: center;
+    margin-bottom: ${theme.spacing.md};
+`
+
+const FormLogin = styled.div `
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.spacing.sm};
+`
+
+const FormButton = styled.button `
+    background: ${theme.colors.primary_cake};
+    color: ${theme.colors.white};
+    padding: ${theme.spacing.sm};
+    border-radius: ${theme.borderRadius.round};
+    border: none;
+`
+
+const FormInput = styled.input `
+    border: none;
+    padding: ${theme.spacing.sm} ${theme.spacing.xl};
+    border-radius: ${theme.borderRadius.round};
+`
--- src/pages/login_page/LoginPage.jsx
+++ src/pages/login_page/LoginPage.jsx
@@ -1,13 +1,18 @@
 import LoginForm from "../../components/login_form/LoginForm";
 import IMAGES from "../../Images";
+import styled from 'styled-components';
+import { theme } from "../../theme/Theme";
 import './loginPage.css';
 
 function LoginPage() {
+
     return (
         <>
             <div className="login-container">
-                <img src={IMAGES.logo} alt="logo Cake Nest" />
-                <h2>Bienvenue chez nous !</h2>
+                <div className="login-container-element">
+                    <Title>Cake<img className="title-img" src={IMAGES.logo} alt="logo Cake Nest" />Nest</Title>
+                    <SubTitle>Bienvenue chez nous !</SubTitle>
+                </div>
                 <LoginForm />
             </div>
         </>
@@ -15,3 +20,22 @@ function LoginPage() {
 }
 
 export default LoginPage;
+
+const SubTitle = styled.h2 `
+        color: ${theme.colors.white};
+        font-family: 'Pacifico', cursive;
+        font-size: ${theme.fonts.size.P5};
+        border-bottom: 1px solid ${theme.colors.loginLine};
+        padding: ${theme.spacing.sm};
+        width: 100%;
+        text-align: center;
+    `
+
+const Title = styled.h1 `
+    color: ${theme.colors.primary_cake};
+    font-family: 'Open Sans', sans-serif;
+    text-transform: uppercase;
+    font-size: ${theme.fonts.size.P5};
+    width: 100%;
+    text-align: center;
+`
