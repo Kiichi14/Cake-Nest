143635f7e04218232d57cb118bf0b483cd0de801
Kiichi14 <100412917+Kiichi14@users.noreply.github.com>
2023-11-22 13:51:29 +0100

    F06 AdminSwitch > admin toggle

--- src/components/admin_switch/AdminSwitch.jsx
+++ src/components/admin_switch/AdminSwitch.jsx
@@ -1,19 +1,13 @@
 import styled from "styled-components";
 import { theme } from "../../theme/Theme";
-import { useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import PropTypes from 'prop-types';
 
-function AdminSwitch() {
-
-    const [admin, setAdmin] = useState(false);
-
-    const handleChange = () => {
-        admin === false ? setAdmin(true) : setAdmin(false);
-    }
-
+function AdminSwitch({isAdmin, toggleAdmin}) {
     return (
         <>
-            <AdminDivStyled onClick={handleChange}>
-                {admin === false ?
+            <AdminDivStyled onClick={toggleAdmin}>
+                {isAdmin === false ?
                     <>
                         <div className="switch-admin"></div>
                         <p className="switch-admin-text">Activer le Mode Admin</p>
@@ -21,7 +15,7 @@ function AdminSwitch() {
                 :
                     <>
                         <div className="switch-admin active"></div>
-                        <p className="switch-admin-text">Désactiver le Mode Admin</p>
+                        <p className="switch-admin-text active">Désactiver le Mode Admin</p>
                     </>
                 }
             </AdminDivStyled>
@@ -31,6 +25,14 @@ function AdminSwitch() {
 
 export default AdminSwitch;
 
+// PROPS VALIDATION
+
+
+AdminSwitch.propTypes = {
+    isAdmin: PropTypes.bool,
+    toggleAdmin: PropTypes.func
+};
+
 const AdminDivStyled = styled.div `
     background : ${theme.colors.background_dark};
     display: flex;
@@ -39,14 +41,25 @@ const AdminDivStyled = styled.div `
     justify-content: space-between;
     color: ${theme.colors.white};
     font-family: 'Open Sans';
+    border-radius: 50px;
+    padding: 10px;
     cursor: pointer;
     & .switch-admin {
         background: ${theme.colors.primary_cake};
-        width: 50px;
-        height: 50px;
+        width: 30px;
+        height: 30px;
         border-radius: 50%;
+        order: 1;
+    }
+    & .switch-admin-text {
+        order: 2;
     }
     & .switch-admin.active {
-        background: ${theme.colors.red}
+        background: ${theme.colors.red};
+        order: 2;
     }
+    & .switch-admin-text.active {
+        order: 1;
+    }
+    
 `
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -3,14 +3,41 @@ import { theme } from '../../theme/Theme';
 import MainIcon from '../main_icon/MainIcon';
 import ProfileWidget from '../profile_widget/ProfileWidget';
 import AdminSwitch from '../admin_switch/AdminSwitch';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function Navbar() {
+
+    const [admin, setAdmin] = useState(false);
+
+    const handleChange = () => {
+        if(admin === false) {
+            setAdmin(true);
+            toast.success('Admin Activé', {
+                position: toast.POSITION.BOTTOM_RIGHT,
+                theme: "dark",
+            });
+        } else {
+            setAdmin(false);
+            toast.info('Admin Désactivé', {
+                position: toast.POSITION.BOTTOM_RIGHT,
+                theme: "dark",
+            })
+        }
+    }
+
     return (
-        <NavBar>
-            <MainIcon />
-            <AdminSwitch />
-            <ProfileWidget />
-        </NavBar>
+        <>
+            <NavBar>
+                <MainIcon />
+                <div className="container-profile-widget">
+                    <AdminSwitch isAdmin={admin} toggleAdmin={handleChange}/>
+                    <ProfileWidget />
+                </div>
+            </NavBar>
+            <ToastContainer />
+        </>
     )
 }
 
@@ -22,4 +49,8 @@ const NavBar = styled.nav `
     background-color: ${theme.colors.white};
     border-radius: ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound} 0px 0px;
     padding: ${theme.spacing.md};
+    & .container-profile-widget {
+        display: flex;
+        gap: ${theme.spacing.md}
+    }
 `
